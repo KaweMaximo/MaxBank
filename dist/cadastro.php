a6fa7ff65099b1d86a872e61fcc685891d8fba57
Kawe Maximo <kawemaximogodoi@hotmail.com>
2022-06-09 21:41:37 -0300

    Adicionando PHP Conect

---
+++ dist/cadastro.php
@@ -0,0 +1,40 @@
+<?php
+
+$login = $_POST['login'];
+$senha = MD5($_POST['senha']);
+$connect = mysql_connect('nome_do_servidor','nome_de_usuario','senha');
+$db = mysql_select_db('nome_do_banco_de_dados');
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysql_query($query_select,$connect);
+$array = mysql_fetch_array($select);
+$logarray = $array['login'];
+
+  if($login == "" || $login == null){
+    echo"<script language='javascript' type='text/javascript'>
+    alert('O campo login deve ser preenchido');window.location.href='
+    cadastro.html';</script>";
+
+    }else{
+      if($logarray == $login){
+
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Esse login já existe');window.location.href='
+        cadastro.html';</script>";
+        die();
+
+      }else{
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+        $insert = mysql_query($query,$connect);
+
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Usuário cadastrado com sucesso!');window.location.
+          href='login.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Não foi possível cadastrar esse usuário');window.location
+          .href='cadastro.html'</script>";
+        }
+      }
+    }
+?>
--- dist/redirect.php
+++
@@ -1,2 +0,0 @@
-#redirect.php
-#receber todas as chamadas no arquivo [here]
